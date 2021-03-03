7b953442968179cc1fd1ee3b19f018f7f9f25ffc
柳達也 <nakku@nakkunoMacBook-Air.local>
2021-03-03 15:05:32 +0900

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,9 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="post_list"),
-    path()
+    path("detail/<int:pk>", views.PostDetail.as_view(), name="post_detail"),
+    path("comment/<int:pk>/", views.CommentCreate.as_view(), name="comment_create"),
+    path("category/<int:pk>/", views.PostCategoryList.as_view(), name="post_category_list"),
+    path("tag/<int:pk>/", views.PostTagList.as_view(), name="post_tag_list"),
+    path("create/", views.PostCreate.as_view(), name="post_create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,77 @@
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect
 from django.views import generic
-from .models import Post
+from .forms import CommentCreateForm, PostSearchForm, PostCreateForm
+from .models import Post, Comment, Tag, Category
+from django.urls import reverse_lazy
 
 
 class PostList(generic.ListView):
     model = Post
-    ordering = '-created_at'
+    ordering = "-created_at"
+    paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        form = PostSearchForm(self.request.GET or None)
+        if form.is_valid():
+            key_word = form.cleaned_data.get('key_word')
+            if key_word:
+                queryset = queryset.filter(Q(title__icontains=key_word) | Q(text__icontains=key_word))
+
+            category = form.cleaned_data.get('category')
+            if category:
+                queryset = queryset.filter(category=category)
+
+            tags = form.cleaned_data.get('tags')
+            if tags:
+                queryset = queryset.filter(tags__in=tags).distinct()
+
+            user = form.cleaned_data.get('user')
+            if user:
+                queryset = queryset.filter(writer=user)
+
+        return queryset
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+
+
+class CommentCreate(generic.CreateView):
+    model = Comment
+    form_class = CommentCreateForm
+
+    def form_valid(self, form):
+        post_pk = self.kwargs["pk"]
+        post = get_object_or_404(Post, pk=post_pk)
+        comment = form.save(commit=False)
+        comment.target = post
+        comment.save()
+        return redirect("blog:post_detail", pk=post_pk)
+
+
+class PostCategoryList(generic.ListView):
+    model = Post
+    ordering = "-created_at"
+    paginate_by = 1
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk=self.kwargs["pk"])
+        return super().get_queryset().filter(category=category)
+
+
+class PostTagList(generic.ListView):
+    model = Post
+    ordering = "created_at"
+    paginate_by = 1
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs["pk"])
+        return super().get_queryset().filter(tags=tag)
+
+
+class PostCreate(generic.CreateView):
+    model = Post
+    form_class = PostCreateForm
+    success_url = reverse_lazy("blog:post_list")
