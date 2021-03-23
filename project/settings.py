86517413825dc4ad3b20557fbd658d667f4c2a65
柳達也 <nakku@nakkunoMacBook-Air.local>
2021-03-24 04:57:32 +0900

    commit

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from pathlib import Path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = 'login' # ログインしていないときのリダイレクト先
 LOGIN_REDIRECT_URL = 'blog:post_list' # ログイン後のリダイレクト先
 LOGOUT_REDIRECT_URL = 'blog:post_list' # ログアウト後のリダイレクト先
